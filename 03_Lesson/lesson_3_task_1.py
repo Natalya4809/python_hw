a3e1ef9ad58c76f0ea6e4372475648afe895e632
Natalya4809 <nata_pusj@mail.ru>
2026-07-15 23:09:42 +0500

    Внесены исправления после проверки

--- 03_Lesson/lesson_3_task_1.py
+++ 03_Lesson/lesson_3_task_1.py
@@ -2,9 +2,9 @@ from user import User
 
 my_user = User ("Nataly", "Kuznetsova")
 
-print(my_user.get_first_name())
-print(my_user.get_last_name())
-print(my_user.get_init_info())
+print(my_user.first_name)
+print(my_user.last_name)
+
 
 
 
