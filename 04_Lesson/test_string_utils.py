7b4426a5da78d4ded2c7e17b0d06e697408fbba9
Natalya4809 <nata_pusj@mail.ru>
2026-07-30 22:58:48 +0500

    Внесла исправления после проверки

--- 04_Lesson/test_string_utils.py
+++ 04_Lesson/test_string_utils.py
@@ -4,6 +4,7 @@ from string_utils import StringUtils
 
 string_utils = StringUtils()
 
+# Тесты для метода capitalize
 
 @pytest.mark.positive
 @pytest.mark.parametrize("input_str, expected", [
@@ -11,7 +12,7 @@ string_utils = StringUtils()
     ("hello world", "Hello world"),
     ("python", "Python"),
     ("    ", ""),
-    ("  pyton","pyton"),
+    ("  pyton", "Pyton"),
     ("123abc", "123Abc")
 ])
 def test_capitalize_positive(input_str, expected):
@@ -20,40 +21,73 @@ def test_capitalize_positive(input_str, expected):
 
 @pytest.mark.negative
 @pytest.mark.parametrize("input_str, expected", [
-    ("123abc", "123abc"),
-    ("pyton", "pyton"),
+    (" ", ""),
+    ("Pyton", "Pyton"),
     ("05 мая 2026   ", "05 мая 2026"),
-    ("",""),
-    ("12456","123456"),
-    ("   abc","abc")
+    ("", ""),
+    ("12456", "123456"),
+    ("   abc", "abc")
 ])
 def test_capitalize_negative(input_str, expected):
     assert string_utils.capitalize(input_str) == expected
 
 
+# Тесты для метода trim
+
 @pytest.mark.positive
-@pytest.mark.parametrize("str1, str2, result",
-     [("Skypro", "o", True),
-      ("Skypro", "k", True),
-      ("Skypro", "pro", True),])
-def test_strib_positive(str1, str2, result):
-    string_utils=StringUtils()
-    res=stringUtils.contains(str1, str2)
-    assert res == result
+@pytest.mark.parametrize("input_str, expected", [
+    ("  Skypro", "Skypro"),
+    ("    123", "123"),
+])
+
+def test_trim_positive(input_str, expected):
+    assert string_utils.trim(input_str) ==expected
+
+@pytest.mark.negative
+@pytest.mark.parametrize("input_str, expected", [
+    ("", ""),
+    ("    ", ""),
+])
+
+def test_trim_negative(input_str, expected):
+    assert string_utils.trim(input_str) ==expected
+
+# Тесты для метода contains
+
+@pytest.mark.positive
+@pytest.mark.parametrize("string, symbol, expected", [
+    ("Skypro", "S", True),
+    ("Skypro", "o", True),
+    ("Skypro", "x", False),
+])
+def test_contains_positive(string, symbol, expected):
+    assert string_utils.contains(string, symbol) == expected
 
 
 @pytest.mark.negative
-@pytest.mark.parametrize("str1, str2, result", [
-    ("Skypro", "a", false),
-    ("Skypro", "g", false),
-    ("Skypro", "e", felse)])
-def test_strib_negative(str1, str2, result):
-    string_utils = StringUtils
+@pytest.mark.parametrize("string, symbol, expected", [
+    ("Skypro", "a", False),
+    ("Skypro", "g", False),
+    ("Skypro", "e", False)])
+def test_contains_(string, symbol, expected):
+    assert string_utils.contains(string, symbol) == expected
 
 
+# Тесты для метода delete_symbol
 
 
+@pytest.mark.positive
+@pytest.mark.parametrize("string, symbol, expected", [
+    ("Skypro", "p", "Skyro"),
+    ("Skypro", "o", "Skyr")])
+def test_delete_symbol_(string, symbol, expected):
+    assert string_utils.delete_symbol(string, symbol) ==expected
 
 
-
-
+@pytest.mark.negative
+@pytest.mark.parametrize("string, symbol, expected", [
+    ("Skypro", "z", "Skypro"),
+    ("", "a", ""),
+    ("   ", "S", "   ")])
+def test_delete_symbol_(string, symbol, expected):
+    assert string_utils.delete_symbol(string, symbol) ==expected
